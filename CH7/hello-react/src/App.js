32e88ceefdbe81ae652e3735d567f3f1c842d004
leeyongju <dydwn5390@mqnic.com>
2022-01-26 14:37:29 +0900

    ch7

---
+++ CH7/hello-react/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import LifeCycleSample from './LifeCycleSample';
+import { Component } from 'react';
+import ErrorBoundary from './ErrorBoundary';
+
+const getRandomColor = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}></LifeCycleSample>
+        </ErrorBoundary>
+      </div>
+
+    )
+  }
+}
+export default App;
