1275a4816f0daccb3519aca7a47131383994022b
leeyongju <dydwn5390@mqnic.com>
2022-01-26 13:39:59 +0900

    ch6 컴포넌트 반복

---
+++ CH6/hello-react/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import IterationSample from './IterationSample';
+
+const App = () => {
+  return <IterationSample></IterationSample>
+};
+
+export default App;
