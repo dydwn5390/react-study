a550baa7695445c7d2b607e9ede13260fa3aa86f
leeyongju <dydwn5390@mqnic.com>
2022-01-26 14:37:55 +0900

    ch8 hooks usestate

---
+++ CH8/hooks-tutorial/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+import Info from './info';
+
+const App = () => {
+  return <Info />;
+}
+
+export default App;
