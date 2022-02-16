87b1c92400bca0a865d6d51dd52708520d51c0ea
leeyongju <dydwn5390@mqnic.com>
2022-02-16 14:22:30 +0900

    ch16 redux 실습

---
+++ CH16/vanilla-redux/index.js
@@ -0,0 +1,72 @@
+import { createStore } from 'redux';
+
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+    toggle: false,
+    counter: 0
+};
+
+// 리듀서 함수 정의
+// state 가 undefined 일 때는 initialState를 기본값으로 사용 (초기)
+function reducer(state = initialState, action){
+    // action.type 에 따라 다른 작업을 처리함
+    switch (action.type) {
+        case TOGGLE_SWITCH:
+            return {
+                ...state,   // 불변성을 유지 해 주어야 합니다.
+                toggle: !state.toggle
+            };
+        case INCREASE:
+            return {
+                ...state,
+                counter: state.counter + action.difference
+            };
+        case DECREASE:
+            return {
+                ...state,
+                counter: state.counter - 1
+            };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+// render 함수 작성
+const render = () => {
+    const state = store.getState();
+    if (state.toggle) {
+        divToggle.classList.add('active');
+    } else {
+        divToggle.classList.remove('active');
+    }
+
+    // 카운터 처리
+    counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+divToggle.onclick = () => {
+    store.dispatch(toggleSwitch());
+};
+btnIncrease.onclick = () => {
+    store.dispatch(increase(1));
+};
+btnDecrease.onclick = () => {
+    store.dispatch(decrease());
+};
